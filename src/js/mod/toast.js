b22c3a1af708020286a30f53c4654aa62ecfe2f8
yyyh <444510796@qq.com>
2019-06-22 11:28:58 +0800

    toast.js

---
+++ src/js/mod/toast.js
@@ -0,0 +1,33 @@
+// require('less/toast.less');
+
+function toast(msg, time) {
+    this.msg = msg;
+    this.dismissTime = time || 1000; //ms
+    this.creatToast();
+    this.showToast();
+}
+toast.prototype = {
+    creatToast: function () {
+        var tpl = '<div class="toast">' + this.msg + '</div>';
+        this.$toast = $(tpl);
+        $('body').append(this.$toast);
+    },
+    showToast: function () {
+        var self = this;
+        this.$toast.fadeIn(300, function () {
+            setTimeout(() => {
+                self.$toast.fadeOut(300, function () {
+                    self.$toast.remove();
+                });
+            }, self.dismissTime);
+        });
+    }
+}
+
+function Toast(msg, time) {
+    return new toast(msg, time);
+}
+
+Toast('hello')
+
+module.exports.Toast = Toast;
